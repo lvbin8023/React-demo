251f8589fac85a16f1359397d1999ca6cb83e375
lvbin <472245767@qq.com>
2019-02-02 13:38:20 +0800

    组件的类和函数的写法

--- my-project/src/components/NameCard.js
+++ my-project/src/components/NameCard.js
@@ -1,24 +1,45 @@
 import React from 'react';
 
-class NameCard extends React.Component {
-	render() {
-		const {name,professional,number,tags} = this.props;
-		return (
-			<div className='alert alert-success NameCard'>
-				<h4 className='alert-heading'>{name}</h4>
-				<ul>
-					<li>职业：{professional}</li>
-					<li>电话：{number}</li>
-					<hr/>
-					<p>
-						{
-							tags.map((tag,index) => (<span className='badge badge-pill' key={index}>{tag}</span>))
-						}
-					</p>
-				</ul>
-			</div>
-		)
-	}
+//函数的写法
+const NameCard = (props) => {
+	const { name, professional, number, tags } = props;
+  return (
+      <div className='alert alert-success NameCard'>
+			<h4 className='alert-heading'>{name}</h4>
+			<ul>
+				<li>职业：{professional}</li>
+				<li>电话：{number}</li>
+				<hr/>
+				<p>
+					{
+						tags.map((tag,index) => (<span className='badge badge-pill' key={index}>{tag}</span>))
+					}
+				</p>
+			</ul>
+		</div>
+  )
 }
+
+//类的写法
+// class NameCard extends React.Component {
+// 	render() {
+// 		const {name,professional,number,tags} = this.props;
+// 		return (
+// 			<div className='alert alert-success NameCard'>
+// 				<h4 className='alert-heading'>{name}</h4>
+// 				<ul>
+// 					<li>职业：{professional}</li>
+// 					<li>电话：{number}</li>
+// 					<hr/>
+// 					<p>
+// 						{
+// 							tags.map((tag,index) => (<span className='badge badge-pill' key={index}>{tag}</span>))
+// 						}
+// 					</p>
+// 				</ul>
+// 			</div>
+// 		)
+// 	}
+// }
 
 export default NameCard
