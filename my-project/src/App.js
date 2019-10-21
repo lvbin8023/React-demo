a9969cb921ff31201799517db6f031bcddce51f2
lvbin <472245767@qq.com>
2019-10-21 11:11:03 +0800

    喜欢按钮代码优化

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NameCard from './components/NameCard';
-import LikesButton from './components/LikesButton';
-import DigitalClock from './components/DigitalClock';
-import CommentBox from './components/CommentBox';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import NameCard from './components/NameCard'
+import LikesButton from './components/LikesButton'
+import DigitalClock from './components/DigitalClock'
+import CommentBox from './components/CommentBox'
 import CommentList from './components/CommentList'
-const tags = ['HTML','CSS','JavaScript','Bootstrap','jQuery','Vue','React'];
+const tags = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Bootstrap',
+  'jQuery',
+  'Vue',
+  'React'
+]
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      comments: ['this is first reply','this is 2nd reply']
+      comments: ['this is first reply', 'this is 2nd reply']
     }
   }
   onAddComment(comment) {
     this.setState({
-      comments:[...this.state.comments,comment]
+      comments: [...this.state.comments, comment]
     })
   }
   render() {
-    const {comments} = this.state
+    const { comments } = this.state
     return (
       <div className="App">
         <header className="App-header">
           <DigitalClock />
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Here's a simple page built using React.
-          </p>
+          <p>Here's a simple page built using React.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -39,13 +45,23 @@ class App extends Component {
             Learn React
           </a>
           <LikesButton />
-          <NameCard name='lvbin/吕彬' professional='前端工程师' number='18516769029' tags={tags}/>
+          <NameCard
+            name="lvbin/吕彬"
+            professional="前端工程师"
+            number="18516769029"
+            tags={tags}
+          />
           <CommentList comments={comments} />
-          <CommentBox commentsLength={comments.length} onAddComment={(comment)=>{this.onAddComment(comment)}} />
+          <CommentBox
+            commentsLength={comments.length}
+            onAddComment={comment => {
+              this.onAddComment(comment)
+            }}
+          />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- my-project/src/components/likesButton.js
+++ my-project/src/components/likesButton.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React from 'react'
 
 class LikesButton extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			likes:0
-		}
-	}
-	increasLikes() {
-		this.setState({ //更改state只能通过setState方法
-			likes: ++this.state.likes
-		})
-	}
-	render() {
-		return (
-			<div className='likesButton'>
-				<button type='button' className='btn btn-outline-primary' onClick={() => {this.increasLikes()}}>
-					👍 {this.state.likes}
-				</button>
-			</div>
-		)
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      likes: 0
+    }
+  }
+  increasLikes() {
+    this.setState(likeState => {
+      //更改state只能通过setState方法
+      return { likes: ++likeState.likes }
+    })
+  }
+  render() {
+    return (
+      <div className="likesButton">
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={() => {
+            this.increasLikes()
+          }}
+        >
+          <span>👍 {this.state.likes}</span>
+        </button>
+      </div>
+    )
+  }
 }
 
 export default LikesButton
